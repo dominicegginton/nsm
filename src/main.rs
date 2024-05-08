244eff482385d3347df801fa5fde6b69436a6ac9
Dominic Egginton <dominic.egginton@gmail.com>
2024-05-08 18:44:06 +0100

    feat(app): add test cmd for host, plus build & expire-generations cmd for home

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ struct HostCommand {
 #[argh(subcommand)]
 enum HostSubCommand {
     Switch(HostSwitchCommand),
+    Test(HostTestCommand),
     CollectGarbage(HostCollectGarbageCommand),
 }
 
@@ -43,6 +44,23 @@ impl Runnable for HostSwitchCommand {
         let _ = std::process::Command::new("sudo")
             .arg("nixos-rebuild")
             .arg("switch")
+            .arg("--flake")
+            .arg(env::var("NSM_FLAKE").unwrap())
+            .spawn()
+            .expect("failed")
+            .wait();
+    }
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Test the host configuration
+#[argh(subcommand, name = "test")]
+struct HostTestCommand {}
+impl Runnable for HostTestCommand {
+    fn run(&self) {
+        let _ = std::process::Command::new("sudo")
+            .arg("nixos-rebuild")
+            .arg("test")
             .arg("--flake")
             .arg(env::var("NSM_FLAKE").unwrap())
             .spawn()
@@ -99,6 +117,8 @@ struct HomeCommand {
 #[argh(subcommand)]
 enum HomeSubCommand {
     Switch(HomeSwitchCommand),
+    Build(HomeBuildCommand),
+    CollectGarbage(HomeCollectGarbageCommand),
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -117,18 +137,52 @@ impl Runnable for HomeSwitchCommand {
     }
 }
 
+#[derive(FromArgs, PartialEq, Debug)]
+/// Build te home-manager configuration
+#[argh(subcommand, name = "build")]
+struct HomeBuildCommand {}
+impl Runnable for HomeBuildCommand {
+    fn run(&self) {
+        let _ = std::process::Command::new("home-manager")
+            .arg("build")
+            .arg("--flake")
+            .arg(env::var("NSM_FLAKE").unwrap())
+            .spawn()
+            .expect("failed")
+            .wait();
+    }
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Cleanup the home-manager generations
+#[argh(subcommand, name = "collect-garbage")]
+struct HomeCollectGarbageCommand {}
+impl Runnable for HomeCollectGarbageCommand {
+    fn run(&self) {
+        let _ = std::process::Command::new("home-manager")
+            .arg("expire-generations")
+            .arg("0")
+            .spawn()
+            .expect("failed")
+            .wait();
+    }
+}
+
 fn main() {
     if env::var("NSM_FLAKE").is_ok() {
         let args: Args = argh::from_env();
         match args.command {
             Command::Host(HostCommand { subcommand }) => match subcommand {
                 HostSubCommand::Switch(HostSwitchCommand {}) => HostSwitchCommand {}.run(),
+                HostSubCommand::Test(HostTestCommand {}) => HostTestCommand {}.run(),
                 HostSubCommand::CollectGarbage(HostCollectGarbageCommand { generations }) => {
                     HostCollectGarbageCommand { generations }.run()
                 }
             },
             Command::Home(HomeCommand { subcommand }) => match subcommand {
                 HomeSubCommand::Switch(HomeSwitchCommand {}) => HomeSwitchCommand {}.run(),
+                HomeSubCommand::Build(HomeBuildCommand {}) => HomeBuildCommand {}.run(),
+                HomeSubCommand::CollectGarbage(HomeCollectGarbageCommand {}) => HomeCollectGarbageCommand {}.run(),
             },
         }
     } else {
