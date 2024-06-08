5d896963b596b56d3add16c47d826494b7ed76ff
Dominic Egginton <dominic.egginton@gmail.com>
2024-06-08 12:36:19 +0100

    chore(workspace): always use locked pkgs

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
-{
-  pkgs ? import <nixpkgs> {},
-  rustPlatform ? pkgs.rustPlatform,
-}:
-with rustPlatform;
-  buildRustPackage rec {
-    name = "nsm";
-    pname = "nsm";
-    version = "0.1.0";
-    src = ./.;
-    cargoSha256 = "sha256-9mbA7yTzCPEbDUobS2cdSI9sZApBaPSJQqWgFlzgXHU=";
+{pkgs}:
+with pkgs.rustPlatform;
+with pkgs.lib.sources;
+with builtins; let
+  cargoToml = fromTOML (readFile ./Cargo.toml);
+in
+  buildRustPackage {
+    pname = cargoToml.package.name;
+    version = cargoToml.package.version;
+    src = cleanSource ./.;
+    cargoSha256 = "sha256-BV1dHcWly4OREPGioafNt/OQVnNMiZWIsLzqI/85esg=";
     nativeBuildInputs = with pkgs; [
       rustc
       cargo
--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
-{pkgs, ...}:
+{pkgs};
 with pkgs;
-  mkShell rec {
+  mkShell {
     nativeBuildInputs = with pkgs; [
       rustc
       cargo
       rust-analyzer
       rustfmt
     ];
+  shellHook = ''
+      export CARGO_HOME=$PWD/.cargo
+      export PATH=$CARGO_HOME/bin:$PATH
+    '';
   }
